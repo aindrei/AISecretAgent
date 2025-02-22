5e0854bfc214df43dc0b4ef27a7b56c2b2a6b43a
aindrei <aindrei@gmail.com>
2025-02-22 13:50:11 -0800

    Empty api keys file

---
+++ workflows/api_keys.py
@@ -0,0 +1,7 @@
+# Contains the API keys for the various services used in the project
+# Enter the api keys here or in environment variables
+
+brave_search_api_key = None
+mistral_api_key = None
+deepseek_api_key = None
+gemini_api_key = None
